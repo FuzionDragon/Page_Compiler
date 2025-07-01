666cb0c74a3c9c79ad0a48b9557d0ab89bf58684
David L <120467347+FuzionDragon@users.noreply.github.com>
2025-07-01 12:09:24 +0100

    Rough implementation of TF-IDF

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,14 @@ use dirs::home_dir;
 
 mod preprocess;
 mod sqlite_interface;
+mod tf_idf;
 
 use preprocess::preprocess;
+use tf_idf::{
+  tf_idf,
+  all_document_tf_idf,
+  every_term_tf_idf,
+};
 
 const PATH: &str = "dev/rust/page_compiler/src/data.db";
 
@@ -30,7 +36,19 @@ async fn main() -> Result<()>{
   let data = vec!("Cats chase mice, dogs bark loudly, and birds fly south in winter.".to_string(), "Running quickly exhausted him, but he kept jogged until he collapsed on the grass.".to_string());
   println!("Before: {:?}", data.clone());
   let processed = preprocess(data);
-  println!("After: {:?}", processed);
+  println!("After: {:?}", processed.clone());
 
+  let tf_idf_0 = tf_idf("cat".to_string(), processed[0].clone(), processed.clone());
+  let tf_idf_1 = tf_idf("cat".to_string(), processed[1].clone(), processed.clone());
+  println!("Cat in Doc 0: {tf_idf_0}");
+  println!("Cat in Doc 1: {tf_idf_1}");
+
+  let all_tf_idf = all_document_tf_idf("cat".to_string(), processed.clone());
+  println!("Cat in all: {:?}", all_tf_idf);
+
+  let every_term_0 = every_term_tf_idf(processed[0].clone(), processed.clone());
+  let every_term_1 = every_term_tf_idf(processed[1].clone(), processed.clone());
+  println!("Every term in 0: {:?}", every_term_0);
+  println!("Every term in 1: {:?}", every_term_1);
   Ok(())
 }
---
+++ src/tf_idf.rs
@@ -0,0 +1,60 @@
+use std::collections::{HashMap, HashSet};
+
+pub fn tf_idf(term: String, document: Vec<String>, corpus: Vec<Vec<String>>) -> f32 {
+  tf(term.clone(), document) * idf(term.clone(), corpus)
+}
+
+pub fn all_document_tf_idf(term: String, corpus: Vec<Vec<String>>) -> HashMap<usize, f32> {
+  let mut result: HashMap<usize, f32> = HashMap::new();
+
+  for i in 0..corpus.len() {
+    let document = corpus[i].clone();
+    result.insert(i, tf_idf(term.clone(), document, corpus.clone()));
+  }
+
+  result
+}
+
+pub fn every_term_tf_idf(document: Vec<String>, corpus: Vec<Vec<String>>) -> HashMap<String, f32> {
+  let mut result: HashMap<String, f32> = HashMap::new();
+  let unique_terms = document
+    .clone()
+    .into_iter()
+    .collect::<HashSet<_>>();
+
+  for term in unique_terms {
+    result.insert(term.clone(), tf_idf(term.clone(), document.clone(), corpus.clone()));
+  }
+
+  result
+}
+
+fn tf(search_term: String, document: Vec<String>) -> f32 {
+  let mut search_term_count = 0.;
+  let mut all_term_count = 0.;
+
+  for term in document {
+    if term == search_term {
+      search_term_count += 1.;
+    }
+
+    all_term_count += 1.;
+  }
+
+  search_term_count / all_term_count
+}
+
+fn idf(term: String, corpus: Vec<Vec<String>>) -> f32 {
+  let mut count: f32 = 0.;
+  let mut total_documents: f32 = 0.;
+
+  for document in corpus {
+    if document.contains(&term) {
+      count += 1.;
+    }
+
+    total_documents += 1.;
+  };
+
+  (total_documents / count).log(10.0)
+}
